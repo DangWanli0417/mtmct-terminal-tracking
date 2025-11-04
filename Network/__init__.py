f82d8a5d50f163fd94c0351aeee060a65000e614
DangWanli0417 <1049894131@qq.com>
2025-11-04 12:27:47 +0800

    Add files via upload

---
+++ Network/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time: 2020/11/10 10:34
+# @Author: libenchao
+# @Contact: libenchao@caacsri.com
+# @FileName: __init__.py.py
+
+from .osnet_ain import osnet_ain_x1_0
+from .resnet import resnet50_attri
+
+__all__ = ['osnet_ain_x1_0', 'resnet50_attri']
